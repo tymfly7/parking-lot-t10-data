98ab0a3b1e2f5c802e9c38a4a558894b78e64386
slavajda02 <slavajda@gmail.com>
2026-03-10 06:33:37 +0100

    9-Mar data added

--- PYTHON/camera.py
+++ PYTHON/camera.py
@@ -9,16 +9,16 @@ camera_config = picam2.create_still_configuration({"size" : (3200, 1800)})
 picam2.configure(camera_config)
 picam2.start()
 sleep(2)
-n = 100
+n = 80
 
-
-if not os.path.isdir("images"):
-    os.mkdir("images")
+folder_name = datetime.now().strftime('%Y-%m-%d')
+folder_name = "../DATA/" + folder_name
+if not os.path.isdir(folder_name):
+    os.mkdir(folder_name)
 
 for x in range(n):
-    # time = datetime.now().strftime('%Y-%m-%d %H_%M_%S')
     time = datetime.now().strftime('%Y-%m-%d_%H%M')
-    picam2.capture_file("images/" + time + ".jpg")
+    picam2.capture_file(folder_name+"/" + time + ".jpg")
     print("Sucessfully captured image in:", time)
     print(f'{x+1} of {n}')
     sleep(600)
--- PYTHON/test_time.py
+++ PYTHON/test_time.py
@@ -2,3 +2,5 @@ from datetime import datetime
 
 time = datetime.now().strftime('%Y-%m-%d %H_%M_%S')
 print(time)
+time = datetime.now().strftime('%Y-%m-%d')
+print(time)
